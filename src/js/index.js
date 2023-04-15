5ce29b9e985c8f20de5ffd3be9e447583ad591bd
guyguzman <guy.guzman.public@gmail.com>
2023-04-15 01:16:06 -0700

    Add Write Doc example

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,5 @@
-import { initializeApp } from "firebase/app";
-import { getFirestore, doc, getDoc, getDocs, collection, query, where } from "firebase/firestore";
+import { initializeApp, getApp } from "firebase/app";
+import { getFirestore, doc, getDoc, getDocs, collection, query, where, addDoc } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 
@@ -14,18 +14,59 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-const db1 = getFirestore(app);
-const db = getFirestore();
+const db = getFirestore(app);
+const defaultStorage = getStorage();
+const defaultFirestore = getFirestore();
+const auth = getAuth();
 
+console.log(getApp().name);
 
-const collectionVehicles = collection(db, "vehicles");
-const snapshotVehicles = await getDocs(collectionVehicles);
-snapshotVehicles.forEach((doc) => {
-    console.log(doc.id, " >= ", doc.data());
+const docRef1 = doc(db, "vehicles", "KVvCtLcHnEqInfNKPgje");
+const docSnap1 = await getDoc(docRef1);
+console.log(docSnap1.data());
+
+const docRef2 = doc(db, "vehicles", "KVvCtLcHnEqInfNKPgje", "makes", "zzZGGkL1gyflRqioOmyc");
+const docSnap2 = await getDoc(docRef2);
+console.log(docSnap2.data());
+
+const docRef3 = collection(db, "vehicles", "KVvCtLcHnEqInfNKPgje", "makes");
+const docSnap3 = await getDocs(docRef3);
+docSnap3.docs.forEach((doc) => {
+    console.log(doc.id, " => ", doc.data());
 });
 
-const vehicles = await getDocs(collection(db, "vehicles"));
-vehicles.docs.forEach((doc) => {
+const docRef4 = doc(db, "vehicles", "KVvCtLcHnEqInfNKPgje");
+const docSnap4 = await getDoc(docRef4);
+console.log(docSnap4.data());
+
+const newDoc = await addDoc(collection(db, "vehicles"), { year: "2019" });
+
+// const docRef5 = collection(db, "vehicles", "KVvCtLcHnEqInfNKPgje", "makes");
+// const docSnap5 = await getDoc(docRef5);
+// console.log(docSnap5.data());
+
+// const collectionVehicles = collection(db, "vehicles");
+// const snapshotVehicles = await getDocs(collectionVehicles);
+// console.log(snapshotVehicles.data());
+
+
+let vehicles = [];
+// getDocs(collectionVehicles).then((vehicleSnapshot) => {
+//     vehicleSnapshot.forEach((vehicle) => {
+//         vehicles.push({...vehicle.data(), id: vehicle.id });
+//     });
+//     console.log(vehicles);
+// });
+
+// getDocs(collectionVehicles).then((vehicleSnapshot) => {
+//     vehicleSnapshot.forEach((vehicle) => { vehicles.push({...vehicle.data(), id: vehicle.id }); });
+// });
+
+
+
+
+const vehicles2 = await getDocs(collection(db, "vehicles"));
+vehicles2.docs.forEach((doc) => {
     console.log(doc.id, " => ", doc.data());
 });
 
