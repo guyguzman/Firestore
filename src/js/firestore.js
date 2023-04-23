75139c398f42eec4a515c84834848f1865708483
guyguzman <guy.guzman.public@gmail.com>
2023-04-23 01:33:11 -0700

    First two tiers saving to database correctly

--- src/js/firestore.js
+++ src/js/firestore.js
@@ -12,6 +12,7 @@ import {
   query,
   where,
   addDoc,
+  setDoc,
   deleteDoc,
   Timestamp,
 } from "https://www.gstatic.com/firebasejs/9.19.1/firebase-firestore.js";
@@ -139,11 +140,9 @@ async function parseJSONInput(jsonArray) {
 
   for (let itemLevel0 of returnDistinct(jsonArray, 0)) {
     let level1ArrayIn = jsonArray.filter((item) => item[0] == itemLevel0);
-    //let documentId1 = await addDocument(["products"], { category: itemLevel0 });
-    let document = await addDoc(collection(db, databaseName), {
+    let document0 = await addDoc(collection(db, databaseName), {
       category: itemLevel0,
     });
-    console.log(document.id);
     level1ArrayIn = returnDistinct(level1ArrayIn, 1);
     if (printDetail) console.log(`...${itemLevel0}`);
     for (let item2 of level1ArrayIn) {
@@ -151,7 +150,16 @@ async function parseJSONInput(jsonArray) {
         .filter((item) => item[0] == itemLevel0)
         .filter((item) => item[1] == item2);
       if (printDetail) console.log(`......${item2}`);
-      // addDocument([databaseName, itemLevel0], { category: item2 });
+      console.log(document0.id);
+      {
+        let reference1 = doc(
+          collection(db, "products", document0.id, "sections")
+        );
+        let document1 = await setDoc(reference1, {
+          category: item2,
+        });
+      }
+
       level2ArrayIn = returnDistinct(level2ArrayIn, 2);
       level2ArrayOut = [];
       for (let item3 of level1ArrayIn) {
